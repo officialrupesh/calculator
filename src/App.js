ef2bf8a6c56cf2bdbb672464304d6583d936f82f
rautrupesh1 <iamrupeshraut1@gmail.com>
2022-01-25 08:58:16 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import {useReducer} from "react";
+import './style.css';
 import './App.css';
+import DigitButton from "./DigitButton";
+
+
+
+ export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVAULATE: 'evaluate'
+ }
+function reducer(state, {type , payload}){
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || "" }${payload.digit}`
+      } 
+  }
+
+}
 
 function App() {
+  const [{currentOperand, previousOperand, operation} , dispatch]  = useReducer(reducer, {})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <div className="calculator-grid">
+     <div className="output">
+       <div className="previous-operand">{previousOperand} {operation}</div>
+         <div className="current-operand">{currentOperand}</div>
+     </div>
+     <button className="span-two">AC</button>
+     <button>DEL</button>
+     {/* <DigitButton digit="/" dispatch = {dispatch} /> */}
+     {/* <DigitButton digit="1" dispatch = {dispatch} /> */}
+     <button>2</button>
+     <button>3</button>
+     <button>x</button>
+     <button>4</button>
+     <button>5</button>
+     <button>6</button>
+     <button>+</button>
+     <button>7</button>
+     <button>8</button>
+     <button>9</button>
+     <button>-</button>
+     <button>.</button>
+     <button>0</button>
+     <button className="span-two">=</button>
+   </div>
+   </>
   );
 }
 
